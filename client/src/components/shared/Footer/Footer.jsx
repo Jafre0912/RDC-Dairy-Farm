36205b4d0605c1965ce45137b9b14df54877c5e1
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-05-23 02:38:06 +0000

    d

--- client/src/components/shared/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className="w-full text-center bg-green-600 text-white">
-      <p className="text-sm py-2">
-        &copy; {new Date().getFullYear()} FarmFlow. All rights reserved.
-      </p>
-    </div>
-  );
-};
